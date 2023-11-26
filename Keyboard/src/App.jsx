818b42f8d44f3d4f72f2346a101999381dc459c3
chana-rozin <144158506+chana-rozin@users.noreply.github.com>
2023-11-26 02:19:32 +0200

    some fixings -  its work!!

--- Keyboard/src/App.jsx
+++ Keyboard/src/App.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import Keyboard from './components/Keyboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Is this really change</h1>
-        <Keyboard />
-      </div>
+      
+        <Keyboard></Keyboard>
+      
         
     </>
   )
--- Keyboard/src/components/Keyboard.jsx
+++ Keyboard/src/components/Keyboard.jsx
@@ -1,32 +1,14 @@
 import React, { useState} from 'react';
 import  './Keyboard.css'
-export default function Keyboard(props)
+
+export default function Keyboard()
 {
-    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Backspace"];
-    const alphaBetEnglishOrderedByKeyboard = ["q", "w", "r", "t", "y", "u", "i", "o", "p",
-                                            "a", "s", "d", "f", "g", "h", "j", "k", "l", "Enter",
-                                            "z", "x", "c", "v", "b", "n", "m", ".", ","];
-    const alphaBetHebrewOrderedByKeyboard = [".", ",", "ק", "ר", "א", "ט", "ו", "ן", "ם", "פ",
-                                            "ש", "ד", "ג", "כ", "ע", "י", "ח", "ל", "ך", "ף", "Enter",
-                                            "ז", "ס", "ב", "ה", "נ", "מ", "צ", "ת", "ץ"];    
-    const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
-                                "~", "[", "]", "{", "}", "-", "+", "=", "/", ";", "Enter",
-                                "?", ">", "<", ",", ".", '"', "'", ":"
-                                ]; 
-    const activeButtons = [{act: "space", func: handleSpaceKey}, {act: "shift", func: handleShiftKey}, {act: "changeLanguage", func: handleLanguageChange},
-                            {act: "&^@", func: handleSpecialChars}]
-    const spaceCharacters = [" ", "    "];     
-    const [inputText, setInputText] = useState('');
-    const [keyboardState, setKeyboardState] = useState(keysOfAlphabetHebrewButtons);
-    const [isCaps, setIsCaps] = useState(false); 
-    const [isShift, setIsShift] = useState(false); 
-    let currentState = {hebrew: true, english: false, special: false} 
-    
+
     const handleRegularKey = (key) => {
         setInputText(inputText + key);        
     }
 
-    const handleDeleteCharacter = (key) => {
+    const handleDeleteCharacter = () => {
         if (inputText.length === 0) { 
             return; 
         } 
@@ -69,31 +51,53 @@ export default function Keyboard(props)
     }
 
 
-    const keysOfNumbers = numbers.map(key => (
-        <button key={key} onClick={() => key == "Backspace" ? handleDeleteCharacter(key) : handleRegularKey(key)}>
-            {key}
-        </button>
-    )); 
-    
-    
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Backspace"];
+    const alphaBetEnglishOrderedByKeyboard = ["q", "w", "r", "t", "y", "u", "i", "o", "p",
+                                            "a", "s", "d", "f", "g", "h", "j", "k", "l", "Enter",
+                                            "z", "x", "c", "v", "b", "n", "m", ".", ","];
+    const alphaBetHebrewOrderedByKeyboard = [".", ",", "ק", "ר", "א", "ט", "ו", "ן", "ם", "פ",
+                                            "ש", "ד", "ג", "כ", "ע", "י", "ח", "ל", "ך", "ף", "Enter",
+                                            "ז", "ס", "ב", "ה", "נ", "מ", "צ", "ת", "ץ"];    
+    const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+                                "~", "[", "]", "{", "}", "-", "+", "=", "/", ";", "Enter",
+                                "?", ">", "<", ",", ".", '"', "'", ":"
+                                ]; 
+
     const keysOfSpecialChars = specialCharacters.map(key => (
         <button key={key} onClick={() => handleRegularKey(key)}>
             {key}
         </button>
     ));    
-    let keysOfAlphabetEnglishButtons = alphaBetEnglishOrderedByKeyboard.map(key => (
+    const keysOfAlphabetEnglishButtons = alphaBetEnglishOrderedByKeyboard.map(key => (
     <button key={key} onClick={() => key == "Enter" ? handleRegularKey("\n") : handleRegularKey(key)}>
         {key}
     </button>
     ));
-    let keysOfAlphabetHebrewButtons = alphaBetHebrewOrderedByKeyboard.map(key => (
+    const keysOfAlphabetHebrewButtons = alphaBetHebrewOrderedByKeyboard.map(key => (
         <button key={key} onClick={() => key == "Enter" ? handleRegularKey("\n") : handleRegularKey(key)}>
             {key}
         </button>
     ));
+    
+    const keysOfNumbers = numbers.map(key => (
+        <button key={key} onClick={() => key == "Backspace" ? handleDeleteCharacter() : handleRegularKey(key)}>
+            {key}
+        </button>
+    )); 
+    
+
+    const activeButtons = [{act: "space", func: handleSpaceKey}, {act: "shift", func: handleShiftKey}, {act: "changeLanguage", func: handleLanguageChange},
+                            {act: "&^@", func: handleSpecialChars}]
+    const spaceCharacters = [" ", "    "];     
+    const [inputText, setInputText] = useState('');
+    const [keyboardState, setKeyboardState] = useState(keysOfAlphabetHebrewButtons);
+    const [isCaps, setIsCaps] = useState(false); 
+    const [isShift, setIsShift] = useState(false); 
+    let currentState = {hebrew: true, english: false, special: false} 
+    
+    
     return (
         <>
-        <h1>does this work??</h1>
         <div className="textArea">
             {inputText}
         </div>
