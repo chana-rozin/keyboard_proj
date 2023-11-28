a63336906e750841fb3a652a4f594bf0830789c8
dloy2004 <d3231371@gmail.com>
2023-11-29 00:20:01 +0200

    fixing middle line in english

--- Keyboard/src/components/EnglishKeys/EnglishKeys.jsx
+++ Keyboard/src/components/EnglishKeys/EnglishKeys.jsx
@@ -5,33 +5,33 @@ export default function EnglishKeys(props) {
     const firstRow = ["q", "w", "r", "t", "y", "u", "i", "o", "p"];
     const secondRow = ["a", "s", "d", "f", "g", "h", "j", "k", "l", "Enter"];
     const thirdRow = ["z", "x", "c", "v", "b", "n", "m", ".", ","];
-    const keysOfFirstRow = firstRow.map(key => (
+    const KeysOfFirstRow = firstRow.map(key => (
         <button key={key} onClick={() => props.handleFunction(key)}>
             {key}
         </button>
     ));
-    const keysOfSecondRow = secondRow.map(key => (
-        <button key={key} onClick={() => key == "Enter" ? props.handleFunction("\n") : props.handleFunctions.handleRegularKey(key)}>
+    const KeysOfSecondRow = secondRow.map(key => (
+        <button key={key} onClick={() => key == "Enter" ? props.handleFunction("\n") : props.handleFunction(key)}>
             {key}
         </button>
     ));
-    const keysOfThirdRow = thirdRow.map(key => (
+    const KeysOfThirdRow = thirdRow.map(key => (
         <button key={key} onClick={() => props.handleFunction(key)}>
             {key}
         </button>
     ));
 
-    const keysOfFirstRowUpper = firstRow.map(key => (
+    const KeysOfFirstRowUpper = firstRow.map(key => (
         <button key={key.toUpperCase()} onClick={() => props.handleFunction(key.toUpperCase())}>
             {key.toUpperCase()}
         </button>
     ));
-    const keysOfSecondRowUpper = secondRow.map(key => (
+    const KeysOfSecondRowUpper = secondRow.map(key => (
         <button key={key.toUpperCase()} onClick={() => key == "Enter" ? props.handleFunction("\n") : props.handleFunction(key.toUpperCase())}>
             {key.toUpperCase()}
         </button>
     ));
-    const keysOfThirdRowUpper = thirdRow.map(key => (
+    const KeysOfThirdRowUpper = thirdRow.map(key => (
         <button key={key.toUpperCase()} onClick={() => props.handleFunction(key.toUpperCase())}>
             {key.toUpperCase()}
         </button>
@@ -39,13 +39,13 @@ export default function EnglishKeys(props) {
 
     return (
         props.isUpper ? (<>
-            <div className='row-of-chars' id='first-row'>{keysOfFirstRowUpper}</div>
-            <div className='row-of-chars' id='second-row'>{keysOfSecondRowUpper}</div>
-            <div className='row-of-chars' id='third-row'>{keysOfThirdRowUpper}</div>
+            <div className='row-of-chars' id='first-row'>{KeysOfFirstRowUpper}</div>
+            <div className='row-of-chars' id='second-row'>{KeysOfSecondRowUpper}</div>
+            <div className='row-of-chars' id='third-row'>{KeysOfThirdRowUpper}</div>
         </>) : (<>
-            <div className='row-of-chars' id='first-row'>{keysOfFirstRow}</div>
-            <div className='row-of-chars' id='second-row'>{keysOfSecondRow}</div>
-            <div className='row-of-chars' id='third-row'>{keysOfThirdRow}</div>
+            <div className='row-of-chars' id='first-row'>{KeysOfFirstRow}</div>
+            <div className='row-of-chars' id='second-row'>{KeysOfSecondRow}</div>
+            <div className='row-of-chars' id='third-row'>{KeysOfThirdRow}</div>
         </>)
     )
     
